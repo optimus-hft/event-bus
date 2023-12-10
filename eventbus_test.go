08954e7cdb2eccba0e40d9566e56f452ccd84942
mhabedinpour <abedinpourmh@gmail.com>
2023-12-10 21:49:00 +0330

    fix: avoid duplicate sequence numbers and fix late subscriptions

--- eventbus_test.go
+++ eventbus_test.go
@@ -24,7 +24,7 @@ func TestBus_Subscribe(t *testing.T) {
 	cA1, uA1 := bus.Subscribe(topicA)
 	cA2, _ := bus.Subscribe(topicA)
 	cB1, _ := bus.Subscribe(topicB)
-	a1Counter, a2Counter, b1Counter := 0, 0, 0
+	a1Counter, a2Counter, b1Counter, b2Counter := 0, 0, 0, 0
 
 	ctx, ctxCancel := context.WithCancel(context.Background())
 	defer ctxCancel()
@@ -54,11 +54,25 @@ func TestBus_Subscribe(t *testing.T) {
 	bus.Publish(topicA, eventsA[1])
 	bus.Publish(topicB, eventsB[0])
 
+	cB2, _ := bus.Subscribe(topicB)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case <-cB2:
+				b2Counter++
+			}
+		}
+	}()
+	bus.Publish(topicB, eventsB[1])
+
 	time.Sleep(1 * time.Second)
 
 	assert.Equal(t, 1, a1Counter)
 	assert.Equal(t, 2, a2Counter)
-	assert.Equal(t, 1, b1Counter)
+	assert.Equal(t, 2, b1Counter)
+	assert.Equal(t, 1, b2Counter)
 
 	assert.Equal(t, 2, len(bus.subscriptions))
 	assert.Equal(t, 2, len(bus.publishSequences))
